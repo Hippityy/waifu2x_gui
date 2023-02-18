fccf123f333d243764ad3761681c77f9494d67ad
Ryan Xie <64182766+Hippityy@users.noreply.github.com>
2023-02-18 17:55:54 +1100

    isolated flushbar

---
+++ lib/utils/flushbar_helper.dart
@@ -0,0 +1,49 @@
+library waifu_gui.flushbar_helper;
+
+import 'package:another_flushbar/flushbar.dart';
+import 'package:context_holder/context_holder.dart';
+import 'package:flutter/material.dart';
+
+void showInfoFlushbar({required String text, int duration = 2}) {
+  Flushbar flushbar = Flushbar(
+    message: text,
+    icon: const Icon(
+      Icons.info_outline,
+      size: 20.0,
+      color: Colors.blue,
+    ),
+    margin: const EdgeInsets.all(8),
+    maxWidth: 350,
+    duration: Duration(seconds: duration),
+    boxShadows: [
+      BoxShadow(
+        color: Color(0x000).withOpacity(0.4),
+        offset: Offset(0, 0),
+        blurRadius: 3.0,
+      )
+    ],
+    leftBarIndicatorColor: Colors.blue,
+  )..show(ContextHolder.currentContext);
+}
+
+void showErrorFlushbar({required String text, int duration = 2}) {
+  Flushbar flushbar = Flushbar(
+    message: text,
+    icon: const Icon(
+      Icons.warning_amber_outlined,
+      size: 20.0,
+      color: Colors.red,
+    ),
+    margin: const EdgeInsets.all(8),
+    maxWidth: 350,
+    duration: Duration(seconds: duration),
+    boxShadows: [
+      BoxShadow(
+        color: Color(0x000).withOpacity(0.4),
+        offset: Offset(0, 0),
+        blurRadius: 3.0,
+      )
+    ],
+    leftBarIndicatorColor: Colors.red,
+  )..show(ContextHolder.currentContext);
+}
--- lib/utils/shell_adaptor.dart
+++ lib/utils/shell_adaptor.dart
@@ -1,11 +1,9 @@
 library waifu_gui.shell_adaptor;
 
 import 'dart:io';
-import 'package:another_flushbar/flushbar_helper.dart';
-import 'package:context_holder/context_holder.dart';
+import 'package:waifu_gui/utils/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:process_run/shell.dart';
-import 'package:another_flushbar/flushbar.dart';
 
 import 'package:waifu_gui/utils/globals.dart';
 
@@ -20,15 +18,15 @@ void upscale() async {
 
   bool exeExists = await waifuExeExists();
   if (!exeExists) {
-    showSnackBar(
-        duration: 5,
+    showErrorFlushbar(
+        duration: 3,
         text:
             'Waifu2x-ncnn-vulkan.exe not found at \n $directory\\upscaler\\waifu2x-ncnn-vulkan.exe');
     return;
   }
-  showSnackBar(text: 'Running.');
+  showInfoFlushbar(text: 'Running.');
   await shell.run(shellCommand());
-  showSnackBar(
+  showInfoFlushbar(
       text: 'Done. Output to ${outputPath(importedFilesList[0].path)}');
 }
 
@@ -48,25 +46,3 @@ String outputPath(String filePath) {
   return "$fileNameWithoutExtension-output${fileConfig.extension.string}";
 }
 // remove the extension from a input file path
-
-void showSnackBar({required String text, int duration = 2}) {
-  Flushbar flushbar = Flushbar(
-    message: text,
-    icon: const Icon(
-      Icons.info_outline,
-      size: 20.0,
-      color: Colors.blue,
-    ),
-    margin: EdgeInsets.all(8),
-    maxWidth: 350,
-    duration: Duration(seconds: duration),
-    boxShadows: [
-      BoxShadow(
-        color: Color(0x000).withOpacity(0.4),
-        offset: Offset(0, 0),
-        blurRadius: 3.0,
-      )
-    ],
-    leftBarIndicatorColor: Colors.blue,
-  )..show(ContextHolder.currentContext);
-}
